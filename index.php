2f74aac02211a1a17611860490c62d6853e55565
Toyeeb Ganiu <39214719+TokyoTG@users.noreply.github.com>
2020-06-07 01:41:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -24,8 +24,6 @@ use ElephantIO\Engine\SocketIO\Version2X;
 <body>
     <h1>Welcome</h1>
     <button id="alert">Alert Everyone</button>
-<!--     <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js"></script> -->
-    <!-- <script src="."></script> -->
     <script src="/socket.io/socket.io.js"></script>
     <script>
         let btn = document.getElementById('alert');
